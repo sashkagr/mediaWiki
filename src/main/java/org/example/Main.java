071f7b83c1bd1fb0b2c83503af4096fb9d783265
sashkagr <sashka1802@gmail.com>
2024-02-22 18:21:28 +0300

    driver

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
         SpringApplication.run(Main.class, args);
     }
 }
