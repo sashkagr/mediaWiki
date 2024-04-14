398b88bf043601f4aed36119582fdab1bb977498
sashkagr <115288292+sashkagr@users.noreply.github.com>
2024-04-14 21:24:53 +0300

    clean18

--- src/main/java/org/example/mediawiki/aop/ControllerLoggingAspect.java
+++ src/main/java/org/example/mediawiki/aop/ControllerLoggingAspect.java
@@ -1,5 +1,6 @@
 package org.example.mediawiki.aop;
 
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
@@ -9,26 +10,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
 
+@Setter
 @Aspect
 @Component
 @Slf4j
 public class ControllerLoggingAspect {
 
-    private  Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-
-    public void setLogger(Logger logger) {
-        this.logger = logger;
-    }
-
-    @Before("execution(* org.example.mediawiki.controller."
-            + "RequestManager.*(..))")
+    @Before("execution(* org.example.mediawiki.controller.RequestManager.*(..))")
     public void logBefore(final JoinPoint joinPoint) {
         try {
-            if (joinPoint != null) {
+            if (joinPoint != null && joinPoint.getSignature() != null) {
                 logger.info("Method {} called with arguments {}", joinPoint.getSignature().toShortString(), joinPoint.getArgs());
             } else {
-                throw new IllegalArgumentException("JoinPoint is null");
+                throw new IllegalArgumentException("JoinPoint or its signature is null");
             }
         } catch (Exception e) {
             // Handle the exception or log it
@@ -36,19 +32,15 @@ public class ControllerLoggingAspect {
         }
     }
 
-    @AfterReturning(pointcut = "execution(* org.example.mediawiki."
-            + "controller.RequestManager.*(..))", returning = "result")
-    public void logAfterReturning(final JoinPoint joinPoint,
-                                  final Object result) {
+    @AfterReturning(pointcut = "execution(* org.example.mediawiki.controller.RequestManager.*(..))", returning = "result")
+    public void logAfterReturning(final JoinPoint joinPoint, final Object result) {
         try {
-            if (joinPoint != null && result != null) {
-                logger.info("Method {} returned {}",
-                        joinPoint.getSignature().toShortString(), result);
+            if (joinPoint != null && joinPoint.getSignature() != null && result != null) {
+                logger.info("Method {} returned {}", joinPoint.getSignature().toShortString(), result);
+            } else {
+                throw new IllegalArgumentException("JoinPoint, its signature, or result is null");
             }
-            else {
-                throw new IllegalArgumentException("JoinPoint is null");
-            }
-        }catch (Exception e) {
+        } catch (Exception e) {
             // Handle the exception or log it
             logger.error("An error occurred in logAfter advice: {}", e.getMessage());
         }
